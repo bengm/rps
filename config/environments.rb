9576be0d5326ebd80b5d4d1cb0de40738730e4f5
Ben Morris <github@benmorris.net>
2013-08-02 15:09:04 -0400

    env tweak

--- config/environments.rb
+++ config/environments.rb
@@ -3,3 +3,15 @@
 configure :development do
   set :database, 'sqlite:///dev.db'
 end
+configure :production do
+  db = ENV['DATABASE_URL']
+ 
+  ActiveRecord::Base.establish_connection(
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+  )
+end
